d868ea5a938f972a14f9abda86d1df5d985fa7e6
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-03-29 14:22:14 +0700

    Update deb-public.sh

--- deb-public.sh
+++ deb-public.sh
@@ -10,18 +10,50 @@ mkdir -p /var/$fn
 chmod -R 0777 /var/$fn
 chown -R nobody:nobody /var/$fn
 
-read -p "Do you want to create authentication for access samba shared folder? (Y / N)" mode
+read -p "Do you want to create authentication for access samba shared folder? (y / n)" mode
 
 ase "$mode" in
-	Y) do
-		
-	;;	
-
-case "$mode" in
-	Y) do
+	y) do
+		echo "Please insert username?"
+		read $un
+		useradd -m -p -s $un
+		echo "Please insert password?"
+		read $pass
+		smbpasswd -a $un
 		echo "Please insert nethbios name?"
 		read nbm
-		echo "Please insert workgroup name?"
+		echo "Please insert workgroup name? (type WORKGROUP for default)"
+		read wg
+		
+		mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
+		
+		cat > /etc/samba/smb.conf << EOF
+		[global]
+			workgroup = $wg
+			netbios name = $nbm
+			security = user
+			map to guest = Bad User
+
+		[$fn]
+			comment = Anonymous File Server Share
+			path = /var/$fn
+			browsable =yes
+			writable = yes
+			guest ok = no
+			read only = no
+		EOF
+
+		systemctl restart smbd.service
+		ufw allow samba
+		testpar
+	;;	
+	break
+	
+case "$mode" in
+	n) do
+		echo "Please insert nethbios name?"
+		read nbm
+		echo "Please insert workgroup name? (type WORKGROUP for default)"
 		read wg
 		mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
 		cat > /etc/samba/smb.conf << EOF
@@ -45,3 +77,4 @@ case "$mode" in
 		ufw allow samba
 		testpar
 	;;
+	break
