d57bd34eef57363ffa5e4034b2326b8baa78e42b
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-03-29 17:39:49 +0700

    Update deb-public.sh

--- deb-public.sh
+++ deb-public.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+apt update
 apt install samba samba-common -y
-ufw allow 139
-ufw allow 445
 
 echo "Please insert shared folder name?"
 read fn
@@ -10,9 +9,15 @@ mkdir -p /var/$fn
 chmod -R 0777 /var/$fn
 chown -R nobody:nobody /var/$fn
 
-read -p "Do you want to create authentication for access samba shared folder? (y / n)" mode
+mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
 
-ase "$mode" in
+echo "Please insert nethbios name?"
+read nbn
+echo "Please insert workgroup name? (type WORKGROUP for default)"
+read wg
+
+read -p "Do you want to create authentication for access samba shared folder? (y / n)" mode
+case "$mode" in
 	y) do
 		echo "Please insert username?"
 		read $un
@@ -20,19 +25,12 @@ ase "$mode" in
 		echo "Please insert password?"
 		read $pass
 		smbpasswd -a $un
-		echo "Please insert nethbios name?"
-		read nbm
-		echo "Please insert workgroup name? (type WORKGROUP for default)"
-		read wg
-		
-		mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
-		
+				
 		cat > /etc/samba/smb.conf << EOF
 		[global]
 			workgroup = $wg
-			netbios name = $nbm
+			netbios name = $nbn
 			security = user
-			map to guest = Bad User
 
 		[$fn]
 			comment = Anonymous File Server Share
@@ -42,24 +40,14 @@ ase "$mode" in
 			guest ok = no
 			read only = no
 		EOF
-
-		systemctl restart smbd.service
-		ufw allow samba
-		testpar
 	;;	
-	break
 	
 case "$mode" in
 	n) do
-		echo "Please insert nethbios name?"
-		read nbm
-		echo "Please insert workgroup name? (type WORKGROUP for default)"
-		read wg
-		mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
 		cat > /etc/samba/smb.conf << EOF
 		[global]
 			workgroup = $wg
-			netbios name = $nbm
+			netbios name = $nbn
 			security = user
 			map to guest = Bad User
 
@@ -72,9 +60,9 @@ case "$mode" in
 			read only = no
 			force user = nobody
 		EOF
-
-		systemctl restart smbd.service
-		ufw allow samba
-		testpar
 	;;
-	break
+	
+systemctl restart smbd.service
+ufw allow samba
+testpar
+
