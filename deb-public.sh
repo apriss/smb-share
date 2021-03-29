7e37af351faafc19f924245e02c11e3d4e04eaf4
Apriansyah Ramadhan <79840869+apriss@users.noreply.github.com>
2021-03-29 13:36:32 +0700

    Update deb-public.sh

--- deb-public.sh
+++ deb-public.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt install samba samba-common -y
+ufw allow 139
+ufw allow 445
 
 echo "Please insert shared folder name?"
 read fn
@@ -8,28 +10,38 @@ mkdir -p /var/$fn
 chmod -R 0777 /var/$fn
 chown -R nobody:nobody /var/$fn
 
-echo "Please insert nethbios name?"
-read nbm
-echo "Please insert workgroup name?"
-read wg
-mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
-cat > /etc/samba/smb.conf << EOF
-[global]
-        workgroup = $wg
-        netbios name = $nbm
-        security = user
-	map to guest = Bad User
-	
-[$fn]
-        comment = Anonymous File Server Share
-        path = /var/$fn
-        browsable =yes
-        writable = yes
-        guest ok = yes
-        read only = no
-        force user = nobody
-EOF
+read -p "Do you want to create authentication for access samba shared folder? (Y / N)" mode
 
-systemctl restart smbd.service
-ufw allow samba
-testparm
+ase "$mode" in
+	Y) do
+		
+	;;	
+
+case "$mode" in
+	Y) do
+		echo "Please insert nethbios name?"
+		read nbm
+		echo "Please insert workgroup name?"
+		read wg
+		mv /etc/samba/smb.conf /etc/samba/smb.conf.ori
+		cat > /etc/samba/smb.conf << EOF
+		[global]
+			workgroup = $wg
+			netbios name = $nbm
+			security = user
+			map to guest = Bad User
+
+		[$fn]
+			comment = Anonymous File Server Share
+			path = /var/$fn
+			browsable =yes
+			writable = yes
+			guest ok = yes
+			read only = no
+			force user = nobody
+		EOF
+
+		systemctl restart smbd.service
+		ufw allow samba
+		testpar
+	;;
